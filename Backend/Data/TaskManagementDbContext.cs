a6da76efdac79426268fe6caba64f3063ac235e2
Danie Beets <danie.beets@gmail.com>
2024-07-13 10:35:17 +0200

    Add identity models to database

--- Backend/Data/TaskManagementDbContext.cs
+++ Backend/Data/TaskManagementDbContext.cs
@@ -1,9 +1,11 @@
 ﻿using Backend.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 
 namespace Backend.Data
 {
-    public class TaskManagementDbContext(DbContextOptions<TaskManagementDbContext> options) : DbContext(options)
+    public class TaskManagementDbContext(DbContextOptions<TaskManagementDbContext> options) : IdentityDbContext<IdentityUser>(options)
     {
         public DbSet<TaskItem> Tasks { get; set; }
     }
