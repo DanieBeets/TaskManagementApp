69c79dd9f3b57c3eb8f5ca7b233dad670398a030
Danie Beets <danie.beets@gmail.com>
2024-07-13 10:01:45 +0200

    Adding model and dbContext

---
+++ Backend/Data/TaskManagementDbContext.cs
@@ -0,0 +1,10 @@
+﻿using Backend.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Backend.Data
+{    
+    public class TaskManagementDbContext(DbContextOptions<TaskManagementDbContext> options) : DbContext(options)
+    {
+        public DbSet<TaskItem> Tasks { get; set; }
+    }
+}
---
+++ Backend/Models/TaskItem.cs
@@ -0,0 +1,12 @@
+﻿namespace Backend.Models
+{
+    public class TaskItem
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public DateTime DueDate { get; set; }
+        public string Priority { get; set; }
+        public string Status { get; set; }
+    }
+}
