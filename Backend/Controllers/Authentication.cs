63772129c8bde14198c4390b0f2f8d5beb8ea861
Danie Beets <danie.beets@gmail.com>
2024-07-13 11:12:16 +0200

    Authentication Controller

---
+++ Backend/Controllers/Authentication.cs
@@ -0,0 +1,104 @@
+﻿using Backend.Models.Auth;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+namespace Backend.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class AuthController(
+        UserManager<IdentityUser> userManager,
+        SignInManager<IdentityUser> signInManager,
+        Logger<AuthController> logger,
+        IConfiguration configuration) : ControllerBase
+    {
+        private readonly UserManager<IdentityUser> _userManager = userManager;
+
+        private readonly SignInManager<IdentityUser> _signInManager = signInManager;
+
+        private readonly ILogger<AuthController> _logger = logger;
+
+        private readonly IConfiguration _configuration = configuration;
+
+        [HttpPost("register")]
+        public async Task<IActionResult> Register([FromBody] Register model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var user = new IdentityUser
+            {
+                UserName = model.Email,
+                Email = model.Email
+            };
+
+            var result = await _userManager.CreateAsync(user, model.Password);
+
+            if (result.Succeeded)
+            {
+                return Ok(new { result = "User created successfully" });
+            }
+            else
+            {
+                return BadRequest(result.Errors);
+            }
+        }
+
+        [HttpPost("signin")]
+        public async Task<IActionResult> SignIn([FromBody] SignIn model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var result = await _signInManager.PasswordSignInAsync(model.Email, model.Password, false, false);
+
+            if (result.Succeeded)
+            {
+                var user = await _userManager.FindByEmailAsync(model.Email);
+                if (user != null)
+                {
+                    var token = GenerateJwtToken(user);
+                    return Ok(new { token });
+                }
+                else
+                {
+                    _logger.LogWarning("Could not find user with email {Email} while signing in", model.Email);
+                }
+            }
+                        
+            return Unauthorized();            
+        }
+
+        private string GenerateJwtToken(IdentityUser user)
+        {
+            var claims = new[]
+            {
+                new Claim(JwtRegisteredClaimNames.Sub, user.Email ?? throw new Exception($"Email not set for userId {user.Id}")),
+                new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString())
+            };
+
+            #pragma warning disable CS8604 // Possible null reference argument.
+            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_configuration["Jwt:Key"]));
+            #pragma warning restore CS8604 // Possible null reference argument.
+
+            var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+
+            var token = new JwtSecurityToken(
+                issuer: _configuration["Jwt:Issuer"],
+                audience: _configuration["Jwt:Audience"],
+                claims: claims,
+                expires: DateTime.Now.AddDays(Convert.ToDouble(_configuration["Jwt:ExpireDays"])),
+                signingCredentials: creds);
+
+            return new JwtSecurityTokenHandler().WriteToken(token);
+        }
+    }
+}
---
+++ Backend/Models/Authentication/SignIn.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Backend.Models.Auth
+{
+    public class SignIn
+    {
+        [Required]
+        public required string Email { get; set; }
+
+        [Required]
+        public required string Password { get; set; }
+    }
+}
