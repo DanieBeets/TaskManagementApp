31f0e3e172384c429b9d5349b27cea0e8d1f4062
Danie Beets <danie.beets@gmail.com>
2024-07-13 10:02:11 +0200

    Sorted usings

--- Backend/Data/TaskManagementDbContext.cs
+++ Backend/Data/TaskManagementDbContext.cs
@@ -2,7 +2,7 @@
 using Microsoft.EntityFrameworkCore;
 
 namespace Backend.Data
-{    
+{
     public class TaskManagementDbContext(DbContextOptions<TaskManagementDbContext> options) : DbContext(options)
     {
         public DbSet<TaskItem> Tasks { get; set; }
